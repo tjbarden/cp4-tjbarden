19e6f83f26959228881bad63b96f439592321350
tjbarden <89928797+tjbarden@users.noreply.github.com>
2021-12-08 16:22:15 -0700

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -21,7 +21,7 @@ app.use(
 const mongoose = require("mongoose");
 
 // connect to the database
-mongoose.connect("mongodb://localhost:27017/museum", {
+mongoose.connect("mongodb://127.0.0.1:27017/museum", {
   useNewUrlParser: true,
 });
 
