d380580536f344fc1f24a7845afcea59da257903
tjbarden <89928797+tjbarden@users.noreply.github.com>
2021-12-08 16:40:59 -0700

    Update Admin.vue

--- front-end/src/views/Admin.vue
+++ front-end/src/views/Admin.vue
@@ -1,41 +1,54 @@
 <template>
-<div class="admin">
-      <h1>The Admin Page!</h1>
+  <div class="admin">
+    <h1>The Admin Page!</h1>
     <div class="heading">
       <div class="circle">1</div>
       <h2>Add an Item</h2>
     </div>
     <div class="add">
       <div class="form">
-        <input v-model="title" placeholder="Title">
+        <div>
+          <input v-model="title" placeholder="Title" />
+        </div>
+        <div>
+          <input v-model="description" placeholder="Description" />
+        </div>
         <p></p>
-        <input v-model="description" placeholder="Description" />
-        <p></p>
-        <input type="file" name="photo" @change="fileChanged">
+        <input type="file" name="photo" @change="fileChanged" />
         <button @click="upload">Upload</button>
       </div>
       <div class="upload" v-if="addItem">
-        <h2>{{addItem.title}}</h2>
-        <h2>{{addItem.description}}</h2>
+        <h2>{{ addItem.title }}</h2>
+        <h2>{{ addItem.description }}</h2>
         <img :src="addItem.path" />
       </div>
     </div>
-      <div class="heading">
+
+    <div class="heading">
       <div class="circle">2</div>
       <h2>Edit/Delete an Item</h2>
     </div>
     <div class="edit">
       <div class="form">
-        <input v-model="findTitle" placeholder="Search">
+        <input v-model="findTitle" placeholder="Search" />
         <div class="suggestions" v-if="suggestions.length > 0">
-          <div class="suggestion" v-for="s in suggestions" :key="s.id" @click="selectItem(s)">{{s.title}}
+          <div
+            class="suggestion"
+            v-for="s in suggestions"
+            :key="s.id"
+            @click="selectItem(s)"
+          >
+            {{ s.title }}
           </div>
         </div>
       </div>
       <div class="upload" v-if="findItem">
-        <input v-model="findItem.title">
-        <p></p>
-        <textarea v-model="findItem.description"/>
+        <div>
+          <input v-model="findItem.title" />
+        </div>
+        <div>
+          <textarea v-model="findItem.description" />
+        </div>
         <p></p>
         <img :src="findItem.path" />
       </div>
@@ -44,85 +57,15 @@
         <button @click="editItem(findItem)">Edit</button>
       </div>
     </div>
-
-</div>
+  </div>
 </template>
 
-<style scoped>
-/* Suggestions */
-.suggestions {
-  width: 200px;
-  border: 1px solid #ccc;
-}
-
-.suggestion {
-  min-height: 20px;
-}
-
-.suggestion:hover {
-  background-color: #5BDEFF;
-  color: #fff;
-}
-
-.image h2 {
-  font-style: italic;
-  font-size: 1em;
-}
-
-.heading {
-  display: flex;
-  margin-bottom: 20px;
-  margin-top: 20px;
-}
-
-.heading h2 {
-  margin-top: 8px;
-  margin-left: 10px;
-}
-
-.add,
-.edit {
-  display: flex;
-}
-
-.circle {
-  border-radius: 50%;
-  width: 18px;
-  height: 18px;
-  padding: 8px;
-  background: #333;
-  color: #fff;
-  text-align: center
-}
-
-/* Form */
-input,
-textarea,
-select,
-button {
-  font-family: 'Montserrat', sans-serif;
-  font-size: 1em;
-}
-
-.form {
-  margin-right: 50px;
-}
-
-/* Uploaded images */
-.upload h2 {
-  margin: 0px;
-}
-
-.upload img {
-  max-width: 300px;
-}
-</style>
-
 <script>
-import axios from 'axios';
+import axios from "axios";
+// is this good practice?
 export default {
-  name: 'Admin',
-    data() {
+  name: "Admin",
+  data() {
     return {
       title: "",
       description: "",
@@ -131,13 +74,15 @@ export default {
       items: [],
       findTitle: "",
       findItem: null,
-    }
+    };
   },
-    computed: {
+  computed: {
     suggestions() {
-      let items = this.items.filter(item => item.title.toLowerCase().startsWith(this.findTitle.toLowerCase()));
+      let items = this.items.filter((item) =>
+        item.title.toLowerCase().startsWith(this.findTitle.toLowerCase())
+      );
       return items.sort((a, b) => a.title > b.title);
-    }
+    },
   },
   methods: {
     selectItem(item) {
@@ -145,31 +90,7 @@ export default {
       this.findItem = item;
     },
     fileChanged(event) {
-      this.file = event.target.files[0]
-    },
-    async upload() {
-      try {
-        const formData = new FormData();
-        formData.append('photo', this.file, this.file.name)
-        let r1 = await axios.post('/api/photos', formData);
-        let r2 = await axios.post('/api/items', {
-          title: this.title,
-          path: r1.data.path,
-          description:this.description,
-        });
-        this.addItem = r2.data;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    async getItems() {
-      try {
-        let response = await axios.get("/api/items");
-        this.items = response.data;
-        return true;
-      } catch (error) {
-        console.log(error);
-      }
+      this.file = event.target.files[0];
     },
     async deleteItem(item) {
       try {
@@ -180,6 +101,22 @@ export default {
       } catch (error) {
         console.log(error);
       }
+    },
+    async upload() {
+      try {
+        const formData = new FormData();
+        formData.append("photo", this.file, this.file.name);
+        let r1 = await axios.post("/api/photos", formData);
+        let r2 = await axios.post("/api/items", {
+          title: this.title,
+          path: r1.data.path,
+          description: this.description,
+        });
+        this.addItem = r2.data;
+      } catch (error) {
+        console.log(error);
+      }
+      this.getItems();
     },
     async editItem(item) {
       try {
@@ -194,6 +131,15 @@ export default {
         console.log(error);
       }
     },
+    async getItems() {
+      try {
+        let response = await axios.get("/api/items");
+        this.items = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
   created() {
     this.getItems();
@@ -202,3 +148,61 @@ export default {
 </script>
 
 
+<style scoped>
+.image h2 {
+  font-style: italic;
+  font-size: 1em;
+}
+.heading {
+  display: flex;
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
+.heading h2 {
+  margin-top: 8px;
+  margin-left: 10px;
+}
+.add,
+.edit {
+  display: flex;
+}
+.circle {
+  border-radius: 50%;
+  width: 18px;
+  height: 18px;
+  padding: 8px;
+  background: #333;
+  color: #fff;
+  text-align: center;
+}
+/* Form */
+input,
+textarea,
+select,
+button {
+  font-family: "Montserrat", sans-serif;
+  font-size: 1em;
+}
+.form {
+  margin-right: 50px;
+}
+/* Uploaded images */
+.upload h2 {
+  margin: 0px;
+}
+.upload img {
+  max-width: 300px;
+}
+/* Suggestions */
+.suggestions {
+  width: 200px;
+  border: 1px solid #ccc;
+}
+.suggestion {
+  min-height: 20px;
+}
+.suggestion:hover {
+  background-color: #5bdeff;
+  color: #fff;
+}
+</style>
