2f3e181f64e78ff8d97725dd8b8e9832d178a21b
tjbarden <tjbarden22@gmail.com>
2021-12-10 12:48:28 -0700

    URL try again

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -26,7 +26,7 @@
   </div>
   <div class="footer">
     <router-link to="/admin">Admin</router-link>
-    <p>TJ Barden  - <a target="blank" class="github-link" href="https://github.com/BYUCS260/museum-tjbarden">Github Repository</a></p>
+    <p>TJ Barden  - <a target="blank" class="github-link" href="https://github.com/tjbarden/cp4-tjbarden">Github Repository</a></p>
     <p></p>
   </div>
 </div>
